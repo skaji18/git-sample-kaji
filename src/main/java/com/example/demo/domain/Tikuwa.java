947c707bda3d7b2461943df0367e2f4126b1f24f
kazumasa.kurodani <kazumasa.kurodani@rakus.co.jp>
2017-06-14 14:23:58 +0900

    競合テスト.

--- src/main/java/com/example/demo/domain/Tikuwa.java
+++ src/main/java/com/example/demo/domain/Tikuwa.java
@@ -1,24 +1,29 @@
 package com.example.demo.domain;
 
+/**
+ * 
+ * @author {kazumasa.kurodani}
+ *
+ */
 public class Tikuwa {
 
-	//private String name;
+	private String name;
 	private Integer price;
 
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
+	public String getName() {
+		return name;
+	}
 
-	// public Integer getPrice() {
-	// return price;
-	// }
-	//
-	// public void setPrice(Integer price) {
-	// this.price = price;
-	// }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
 
 }
